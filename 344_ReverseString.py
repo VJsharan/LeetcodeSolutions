a9a926692d2a1ac76d2b15c0bc0b2c4359a4ced2
VijaySharanV <117385197+VJsharan@users.noreply.github.com>
2025-01-08 13:02:51 +0530

    Solved 344 in python

---
+++ 344_ReverseString.py
@@ -0,0 +1,7 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        temp = s.reverse()
+        s=temp
+        return s
+#pretty simple just reverse the string store it in a temp var, 
+#then swap them using the thus mentioned temp and return the modified i/p
