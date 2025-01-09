fb289a06725169c7a423ac83b6df26c155cd69f5
VijaySharanV <117385197+VJsharan@users.noreply.github.com>
2025-01-09 23:54:13 +0530

    Solved 448 in python

--- Find All Numbers Disappeared in an Array.py
+++ Find All Numbers Disappeared in an Array.py
@@ -11,4 +11,5 @@ class Solution:
 
 #did it on my own less fucking go
 #sorts the array, converts it into a set, calculates the sum, 
+# the + 1 is used so that the last is not left out, since in range(1,10), does only from 1-9, we need all 10 so adding the 1 to make it
 #goes through all the elements from 1 - given n and checks if the current i is present in the set, if it is not, then its added to the result array ret
